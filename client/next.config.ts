9a98099cdeb67be6d05a26ae05533db5ae149139
SØØRAJ CHAKRAVARTHY S <soorajchakravarthy3s@gmail.com>
2026-03-08 18:08:44 +0530

    Update next.config.ts

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: 'export',        // generates static files
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
